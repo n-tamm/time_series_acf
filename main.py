47bcf2a5dae255b712f56f8a878ba2ba4477fedd
Noah Tamminga <Noah Tamminga>
2025-11-02 09:15:28 -0500

    Using class to try to get pass pylint param error

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 """
 Main: 
-    main: Loads a time series, computes its autocorrelation function,
+    time_series_acf: Loads a time series, computes its autocorrelation function,
     saves an ACF plot, and displays the plot
 """
 from pathlib import Path
+from dataclasses import dataclass
 from typing import Optional
 import matplotlib.pyplot as plt
 from functions.analysis import compute_acf
@@ -11,15 +12,19 @@ from functions.data_loader import load_time_series
 from functions.plotter import plot_acf
 from functions.utils import check_dir
 
-def time_series_acf(
-    input_file: str,
-    datetime_col: Optional[str],
-    value_col: str,
-    nlags: int,
-    output_dir: str,
-    plot_name: str = "acf_plot",
-    datetime_format: Optional[str] = None,
-) -> None:
+# Using class to get around pylint param error
+@dataclass
+class ACF:
+    input_file: str
+    datetime_col: Optional[str]
+    value_col: str
+    nlags: int
+    output_dir: str
+    plot_name: str = "acf_plot"
+    datetime_format: Optional[str] = None
+
+# Function uses help class ACF to control it's inputs
+def time_series_acf(config: ACF) -> None:
     """
     :param input_file: Path to the CSV file containing the time series
     :param datetime_col: Name of the datetime column
@@ -30,22 +35,18 @@ def time_series_acf(
     :datetime_format: Format to convert datetime column
     :return: None
     """
-    out_path = Path(output_dir)
+    out_path = Path(config.output_dir)
     check_dir(out_path)
-
     series = load_time_series(
-        input_file,
-        datetime_col=datetime_col,
-        value_col=value_col,
-        datetime_format=datetime_format
+        config.input_file,
+        datetime_col=config.datetime_col,
+        value_col=config.value_col,
+        datetime_format=config.datetime_format
     )
-
-    acf_vals = compute_acf(series, nlags)
-
-    fig_path = out_path / f"{plot_name}.png"
+    acf_vals = compute_acf(series, config.nlags)
+    fig_path = out_path / f"{config.plot_name}.png"
     plot_acf(acf_vals, fig_path)
     print(f"Saved ACF plot to {fig_path}")
-
     plt.show()
 
 if __name__ == "__main__":
@@ -59,12 +60,13 @@ if __name__ == "__main__":
     parser.add_argument("--plot_name", help="Name to give saved plot", default="acf_plot")
     parser.add_argument("--datetime_format", help="Datetime format", default=None)
     args = parser.parse_args()
-    time_series_acf(
+    config = ACF(
         input_file=args.input_file,
-        value_col=args.value_col,
         datetime_col=args.datetime_col,
+        value_col=args.value_col,
         nlags=args.nlags,
         output_dir=args.output_dir,
         plot_name=args.plot_name,
-        datetime_format=args.datetime_format
+        datetime_format=args.datetime_format,
     )
+    time_series_acf(config)
